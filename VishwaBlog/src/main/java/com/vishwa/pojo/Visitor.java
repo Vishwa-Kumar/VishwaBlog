958b4759dbe1d14debe590c6cbcbc8c56556c56e
KITSPL\kh2212 <vishwa.deepak@kony.com>
2017-12-24 18:26:45 +0530

    pojo package included with visitor pojo class

---
+++ VishwaBlog/src/main/java/com/vishwa/pojo/Visitor.java
@@ -0,0 +1,49 @@
+package com.vishwa.pojo;
+
+public class Visitor {
+
+	private String visitorIP;
+	private String visitorDevice;
+	private String visitorLocationLattitude;
+	private String visitorLocationLongitude;
+	private String visitorBrowser;
+	private  String visitorVisitingTime;
+	public String getVisitorIP() {
+		return visitorIP;
+	}
+	public void setVisitorIP(String visitorIP) {
+		this.visitorIP = visitorIP;
+	}
+	public String getVisitorDevice() {
+		return visitorDevice;
+	}
+	public void setVisitorDevice(String visitorDevice) {
+		this.visitorDevice = visitorDevice;
+	}
+	public String getVisitorLocationLattitude() {
+		return visitorLocationLattitude;
+	}
+	public void setVisitorLocationLattitude(String visitorLocationLattitude) {
+		this.visitorLocationLattitude = visitorLocationLattitude;
+	}
+	public String getVisitorLocationLongitude() {
+		return visitorLocationLongitude;
+	}
+	public void setVisitorLocationLongitude(String visitorLocationLongitude) {
+		this.visitorLocationLongitude = visitorLocationLongitude;
+	}
+	public String getVisitorBrowser() {
+		return visitorBrowser;
+	}
+	public void setVisitorBrowser(String visitorBrowser) {
+		this.visitorBrowser = visitorBrowser;
+	}
+	public String getVisitorVisitingTime() {
+		return visitorVisitingTime;
+	}
+	public void setVisitorVisitingTime(String visitorVisitingTime) {
+		this.visitorVisitingTime = visitorVisitingTime;
+	}
+	
+	
+}
